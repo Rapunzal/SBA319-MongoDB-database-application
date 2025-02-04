b7854b9ff7aec65926f34524ff3ff2b06bb5ce8a
Priyanka <priyankagjune@gmail.com>
2025-02-03 19:52:47 -0700

    added comments user and product routes

--- routes/product.js
+++ routes/product.js
@@ -14,16 +14,22 @@ import {
 
 const router = express.Router();
 
+//Get Product route
 router.get("/", getProducts);
 
+//get product by id route
 router.get("/product/:id", getProductById);
 
+//Add product route
 router.post("/", addProduct);
 
+//Update product route
 router.put("/:id", updateProduct);
 
+//Delete product route
 router.delete("/:id", deleteProduct);
 
+//Search product by name route
 router.get("/search", searchProductByName);
 
 getIndexes();
--- routes/user.js
+++ routes/user.js
@@ -8,12 +8,16 @@ import {
 
 const router = express.Router();
 
+//Get list of all users route
 router.get("/", getUsers);
 
+//Add user route
 router.post("/", addUser);
 
+//Update user route
 router.put("/:id", updateUser);
 
+//Delete user route
 router.delete("/:id", deleteUser);
 
 export default router;
